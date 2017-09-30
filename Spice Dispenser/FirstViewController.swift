cfefe5b7db4cbffd6a291ba768383d885e43b965
Rakesh Mandhan <rakeshmandhan95@gmail.com>
2017-09-30 14:32:54 -0400

    Initial Commit

---
+++ Spice Dispenser/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Spice Dispenser
+//
+//  Created by Rakesh Mandhan on 2017-09-30.
+//  Copyright © 2017 Rakesh Mandhan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
