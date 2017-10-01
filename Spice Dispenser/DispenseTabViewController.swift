9c7dc3cac777ccaa39ac423b5e01b3574e122e45
Rakesh Mandhan <rakeshmandhan95@gmail.com>
2017-09-30 22:26:18 -0400

    First real commit

---
+++ Spice Dispenser/DispenseTabViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DispenseTabViewController.swift
+//  Spice Dispenser
+//
+//  Created by Rakesh Mandhan on 2017-09-30.
+//  Copyright © 2017 Rakesh Mandhan. All rights reserved.
+//
+
+import Foundation
---
+++ Spice Dispenser/InitialViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InitialViewController.swift
+//  Spice Dispenser
+//
+//  Created by Rakesh Mandhan on 2017-09-30.
+//  Copyright © 2017 Rakesh Mandhan. All rights reserved.
+//
+
+import Foundation
